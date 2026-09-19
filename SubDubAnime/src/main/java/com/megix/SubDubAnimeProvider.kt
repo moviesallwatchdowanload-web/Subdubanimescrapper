c734954f103e34dcb0db45ef2c8cc1af8996849e
moviesallwatchdownload-web <moviesallwatchdowanload@gmail.com>
2026-09-20 02:01:16 +0530

    Fix CineJoy HLS link flow

--- SubDubAnime/src/main/java/com/megix/SubDubAnimeProvider.kt
+++ SubDubAnime/src/main/java/com/megix/SubDubAnimeProvider.kt
@@ -2,25 +2,25 @@ package com.megix
 
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.utils.AppUtils.parseJson
 
-class SubDubAnimeProvider : MainAPI() {
+open class SubDubAnimeProvider : MainAPI() {
 
     override var mainUrl = "https://ok.solarpanelcleaning.cc"
-    override var name = "CineJoy Test"
+    override var name = "CineJoy"
+
+    override val hasMainPage = true
+    override var lang = "hi"
 
     override val supportedTypes = setOf(
         TvType.Movie
     )
 
-    override var lang = "hi"
-
-    override val hasMainPage = true
-
     private val testM3u8 =
         "https://ok.solarpanelcleaning.cc/playlist/_ob6P6lqGyaiEGeoUmDUtg.m3u8"
 
     override val mainPage = mainPageOf(
-        "test" to "CineJoy HLS Test"
+        "test" to "CineJoy Test"
     )
 
     override suspend fun getMainPage(
@@ -29,9 +29,9 @@ class SubDubAnimeProvider : MainAPI() {
     ): HomePageResponse {
 
         val result = newMovieSearchResponse(
-            name = "CineJoy HLS Test",
-            url = "cinejoy-test",
-            type = TvType.Movie
+            "CineJoy HLS Test",
+            "cinejoy-test",
+            TvType.Movie
         ) {
             this.posterUrl = null
         }
@@ -46,7 +46,8 @@ class SubDubAnimeProvider : MainAPI() {
         query: String
     ): List<SearchResponse> {
 
-        if (!"cinejoy hls test".contains(query, ignoreCase = true) &&
+        if (
+            !"cinejoy hls test".contains(query, ignoreCase = true) &&
             !query.contains("cinejoy", ignoreCase = true) &&
             !query.contains("test", ignoreCase = true)
         ) {
@@ -55,9 +56,9 @@ class SubDubAnimeProvider : MainAPI() {
 
         return listOf(
             newMovieSearchResponse(
-                name = "CineJoy HLS Test",
-                url = "cinejoy-test",
-                type = TvType.Movie
+                "CineJoy HLS Test",
+                "cinejoy-test",
+                TvType.Movie
             )
         )
     }
@@ -70,11 +71,17 @@ class SubDubAnimeProvider : MainAPI() {
             return null
         }
 
+        val data = listOf(
+            EpisodeLink(
+                testM3u8
+            )
+        )
+
         return newMovieLoadResponse(
-            name = "CineJoy HLS Test",
-            url = url,
-            type = TvType.Movie,
-            dataUrl = testM3u8
+            "CineJoy HLS Test",
+            url,
+            TvType.Movie,
+            data
         ) {
             this.plot = "CineJoy direct HLS playback test."
         }
@@ -87,18 +94,26 @@ class SubDubAnimeProvider : MainAPI() {
         callback: (ExtractorLink) -> Unit
     ): Boolean {
 
-        callback(
-            newExtractorLink(
-                source = "CineJoy",
-                name = "CineJoy 1080p HLS",
-                url = data,
-                type = ExtractorLinkType.M3U8
-            ) {
-                this.quality = Qualities.P1080.value
-                this.referer = "https://ok.solarpanelcleaning.cc/"
-            }
-        )
+        val sources = parseJson<ArrayList<EpisodeLink>>(data)
+
+        sources.forEach {
+            callback(
+                newExtractorLink(
+                    source = "CineJoy",
+                    name = "CineJoy HLS",
+                    url = it.source,
+                    type = ExtractorLinkType.M3U8
+                ) {
+                    this.quality = Qualities.P1080.value
+                    this.referer = "https://ok.solarpanelcleaning.cc/"
+                }
+            )
+        }
 
         return true
     }
+
+    data class EpisodeLink(
+        val source: String
+    )
 }
